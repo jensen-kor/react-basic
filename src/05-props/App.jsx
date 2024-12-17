240bc340085890bd12636b749d4b337784146684
jensen-kor <lukawa1@naver.com>
2024-12-17 16:45:47 +0900

    2024-12-17

---
+++ src/05-props/App.jsx
@@ -0,0 +1,75 @@
+import PropTypes from "prop-types";
+function Header({ title }) {
+  // 함수형 컴포넌트는 모든 attribute들이 매개변수로 전달
+  console.log(title);
+  return (
+    <header>
+      <h1>
+        <a href="/">{title}</a>
+      </h1>
+    </header>
+  );
+}
+Header.PropTypes = {
+  title: PropTypes.string,
+};
+
+function Nav(topics) {
+  console.log("topics: ", topics);
+  return (
+    <nav>
+      <ol>
+        {/* <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li> */}
+        {topics.map((topic) => (
+          <li key={topic.id}>
+            <a href={"/read/" + topic.id}>{topic.title}</a>
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
+Nav.PropTypes = {
+  topics: PropTypes.array,
+};
+
+function Article({ title, body }) {
+  return (
+    <article>
+      <h2>{title}</h2>
+      {body}
+    </article>
+  );
+}
+Article.PropTypes = {
+  title: PropTypes.string,
+  body: PropTypes.string,
+};
+
+function App() {
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "javascript", body: "jsvascript is ..." },
+  ];
+  return (
+    <div className="App">
+      <Header title="REACT"></Header>
+      {/* topics 배열을 Nav컴포넌트에 전달 */}
+      <Nav topic={topics}></Nav>
+      {/* Article 컴포넌트에 title, body 속성 전달 */}
+      <Article title="Welcome" body="Hello, Web"></Article>
+      <Article title="Hi" body="Hello, React"></Article>
+    </div>
+  );
+}
+
+export default App;
